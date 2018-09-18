670f1bd4e215b6101d8c5eb7a4e5f04b03c5b9cc
Yves Pares <yves.pares@tweag.io>
2018-09-18 14:47:18 +0200

    Actually passing logger params to runLogger

--- src/System/TaskPipeline/Logger.hs
+++ src/System/TaskPipeline/Logger.hs
@@ -24,6 +24,7 @@ data LoggerScribeParams = LoggerScribeParams
   { loggerSeverityThreshold :: Severity
   , loggerVerbosity         :: Verbosity
   }
+  deriving (Eq, Show)
 
 -- | Default LoggerScribeParams shows log message from Info level, with maximum verbosity.
 defaultLoggerScribeParams :: LoggerScribeParams
@@ -35,19 +36,13 @@ runLogger
   => LoggerScribeParams
   -> KatipContextT m a
   -> m a
-runLogger _ x = do
-    handleScribe <- liftIO $ mkHandleScribe ColorIfTerminal stdout InfoS V0
+runLogger (LoggerScribeParams sev verb) x = do
+    handleScribe <- liftIO $ mkHandleScribe ColorIfTerminal stdout sev verb
     let mkLogEnv = liftIO $
             registerScribe "stdout" handleScribe defaultScribeSettings
                 =<< initLogEnv "jinko" "devel"
     bracket mkLogEnv (liftIO . closeScribes) $ \le ->
         runKatipContextT le () "main" $ x
-
--- -- | 'Loc'-tagged logging when using template-haskell.
--- --
--- -- @$(log) InfoS "Hello world"@
--- log :: ExpQ
--- log = logTM
 
 -- | Adds some context that will be used at logging time. See 'katipAddContext'
 addContextToTask
