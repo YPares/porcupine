3d74992cc19e799606a7e06ec9fe1b6fb9ef84dc
Yves Pares <yves.pares@tweag.io>
2018-09-20 11:49:19 +0200

    loadModelTask adds namespace

--- src/System/TaskPipeline/Tasks.hs
+++ src/System/TaskPipeline/Tasks.hs
@@ -3,9 +3,11 @@ module System.TaskPipeline.Tasks
   , module System.TaskPipeline.Resource
   , module System.TaskPipeline.Tasks.LayeredAccess
   , module System.TaskPipeline.Tasks.Options
+  , addContextToTask, addNamespaceToTask
   ) where
 
 import           System.TaskPipeline.ATask
+import           System.TaskPipeline.Logger
 import           System.TaskPipeline.Resource
 import           System.TaskPipeline.Tasks.LayeredAccess
 import           System.TaskPipeline.Tasks.Options
