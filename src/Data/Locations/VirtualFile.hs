d830fa5a1845566708ae0702d980d3e000f2fd1f
Yves Pares <yves.pares@tweag.io>
2018-09-26 20:15:06 +0200

    VirtualFile ctor is private

--- src/Data/Locations/VirtualFile.hs
+++ src/Data/Locations/VirtualFile.hs
@@ -4,10 +4,12 @@ module Data.Locations.VirtualFile
   , BidirSerials, PureSerials, PureDeserials
   , JSONSerial(..), PlainTextSerial(..)
   , Profunctor(..)
-  , VirtualFile(..), BidirVirtualFile, DataSource, DataSink
+  , VirtualFile, BidirVirtualFile, DataSource, DataSink
+  , vfileUsedByDefault, vfileSerials, vfilePath
   , someBidirSerial, somePureSerial, somePureDeserial
   , customPureSerial, customPureDeserial, makeBidir
   , dataSource, dataSink, virtualFile
+  , unusedByDefault
   , vpDeserialToLTPIs, vpSerialToLTPIs
   ) where
 
@@ -20,7 +22,7 @@ import           Data.Void
 -- | A virtual path in the location tree to which we can write @a@ and from
 -- which we can read @b@.
 data VirtualFile a b = VirtualFile
-  { vfileLocation      :: [LocationTreePathItem]
+  { vfilePath          :: [LocationTreePathItem]
   , vfileUsedByDefault :: Bool
   , vfileSerials       :: SerialsFor a b }
 
@@ -36,16 +38,25 @@ type DataSource a = VirtualFile Void a
 -- | A virtual file that's only writable
 type DataSink a = VirtualFile a ()
 
--- | Creates a virtual file from its virtual location and ways to
--- serialize/deserialize the data.
+-- | Creates a virtual file from its virtual path and ways to deserialize the
+-- data.
 dataSource :: [LocationTreePathItem] -> SerialsFor a b -> DataSource b
 dataSource path = virtualFile path . eraseSerials
 
+-- | Creates a virtual file from its virtual path and ways to serialize the
+-- data.
 dataSink :: [LocationTreePathItem] -> SerialsFor a b -> DataSink a
 dataSink path = virtualFile path . eraseDeserials
 
+-- | Creates a virtuel file from its virtual path and ways serialize/deserialize
+-- the data. You should prefer 'dataSink' and 'dataSource' for clarity when the
+-- file is meant to be readonly or writeonly.
 virtualFile :: [LocationTreePathItem] -> SerialsFor a b -> VirtualFile a b
 virtualFile path sers = VirtualFile path True sers
+
+-- | Indicates that the file should be mapped to 'null' by default
+unusedByDefault :: VirtualFile a b -> VirtualFile a b
+unusedByDefault vf = vf{vfileUsedByDefault=False}
 
 -- temporary
 vpDeserialToLTPIs :: VirtualFile a b -> ([LocationTreePathItem], LTPIAndSubtree SerialMethod)
