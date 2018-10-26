3317b7fabd48bc3ff87ca7b098024c787f4aaa9c
Yves Parès (Ywen) <yves.pares@gmail.com>
2018-10-26 14:08:49 +0200

    Updated Example1 so it uses mappingOverStream

--- examples/Example1.hs
+++ examples/Example1.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE DataKinds #-}
 
 import Porcupine.Serials
 import Porcupine.Tasks
 import Porcupine.Run
+import Data.DocRecord
 import Data.Aeson
 import qualified Data.Text as T
 import qualified Data.HashMap.Strict as HM
@@ -28,9 +32,21 @@ analysisFile :: DataSink Analysis
 analysisFile = dataSink ["Outputs", "Analysis"]
                         (somePureSerial JSONSerial)
 
+analyseOneUser :: (LocationMonad m, KatipContext m) => PTask m () ()
+analyseOneUser =
+  loadLast userFile >>> arr computeAnalysis >>> writeData analysisFile
+
 mainTask :: (LocationMonad m, KatipContext m) => PTask m () ()
 mainTask =
-  loadLast userFile >>> arr computeAnalysis >>> writeData analysisFile
+  -- First we get the ids of the users that we want to analyse (we need only one
+  -- field that will contain the list of the ids):
+  getOption ["Settings"] (docField @"userIds" [0::Int] "The user ids to load") >>>
+  arr (map (, ())) >>>  -- The input of analyseOneUser is (), so we need a
+                        -- stream of (Int, ()) pairs
+  listToStreamTask >>>  -- Just a utility if you don't want to explicitly import Streaming
+  -- Finally, we just loop over these ids and call analyseOneUser each time:
+  mappingOverStream_ "userId" Nothing
+    analyseOneUser
 
 computeAnalysis :: User -> Analysis
 computeAnalysis (User name surname _) = Analysis $
@@ -38,4 +54,4 @@ computeAnalysis (User name surname _) = Analysis $
                      ++ [(c,1) | c <- T.unpack surname]
 
 main :: IO ()
-main = runPipelineTask_ "example1" (FullConfig "porcupine.yaml" ".") mainTask
+main = runPipelineTask_ "example1" (FullConfig "porcupine.yaml" "examples/data") mainTask
