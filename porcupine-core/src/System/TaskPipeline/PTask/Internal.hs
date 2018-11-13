7388a7c261eb3913bee0e0b61244202d004e6690
Yves Parès (Ywen) <yves.pares@gmail.com>
2018-11-13 15:59:54 +0100

    Added ArrowFlow implementation for new PTask

--- porcupine-core/src/System/TaskPipeline/PTask/Internal.hs
+++ porcupine-core/src/System/TaskPipeline/PTask/Internal.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeSynonymInstances       #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
 
 module System.TaskPipeline.PTask.Internal where
 
@@ -12,11 +15,21 @@ import           Control.Monad.Trans.Reader
 import           Control.Monad.Trans.Writer
 import           Data.Locations.LocationTree
 import           Data.Locations.LogAndErrors
+import           Katip.Core                       (Namespace)
+import           Katip.Monadic
 import           System.TaskPipeline.ResourceTree
+
+import           Prelude                          hiding (id, (.))
 
 
 type ReqTree = LocationTree VirtualFileNode
 type RunTree m = LocationTree (DataAccessNode m)
+
+data PTaskReaderState m = PTaskReaderState
+  { ptrsKatipContext   :: !LogContexts
+  , ptrsKatipNamespace :: !Namespace
+  , ptrsRunTree        :: !(RunTree m) }
+
 type EffectInFlow m = AsyncA (ReaderT (RunTree m) m)
 
 -- | A task is an Arrow than turns @a@ into @b@. It runs in some monad @m@.
@@ -26,14 +39,32 @@ type EffectInFlow m = AsyncA (ReaderT (RunTree m) m)
 -- are combined with each other, so once the full pipeline has been composed
 -- (through Arrow composition), its 'pTaskResourceTree' will contain the
 -- complete requirements of the pipeline.
-newtype PTask m a b =
-  PTask (AppArrow
-          (Writer ReqTree)  -- The writer layer accumulates the requirements
-          (AppArrow
-            (Reader (RunTree m))  -- The reader layer contains the mapped tree
-            (Flow (EffectInFlow m) TaskRunError))
-                  -- Internally, the flow layer also has effects that depend on
-                  -- a reader layer. The two reader layers are synchronized by
-                  -- 'wrap'
-          a b)
+newtype PTask m a b = PTask
+  (AppArrow
+    (Writer ReqTree)  -- The writer layer accumulates the requirements. It will
+                      -- be used only as an applicative.
+    (AppArrow
+      (Reader (PTaskReaderState m)) -- The reader layer contains the mapped
+                                     -- tree. Will be used only as an
+                                     -- applicative too.
+      (Flow (AsyncA m) TaskRunError))
+    a b)
   deriving (Category, Arrow, ArrowError TaskRunError)
+
+flowToPTask :: Flow (AsyncA m) TaskRunError a b -> PTask m a b
+flowToPTask = PTask . appArrow . appArrow
+
+instance (KatipContext m) => ArrowFlow (EffectInFlow m) TaskRunError (PTask m) where
+  step' props f = flowToPTask $ step' props f
+  stepIO' props f = flowToPTask $ stepIO' props f
+  external f = flowToPTask $ external f
+  external' props f = flowToPTask $ external' props f
+  -- wrap' transmits the Reader state of the PTask down to the flow:
+  wrap' props (AsyncA rdrAct) = PTask $ AppArrow $ pure $ AppArrow $ reader $ \ptrs ->
+    wrap' props $ AsyncA $ \input ->
+      localKatipContext (<> ptrsKatipContext ptrs) $
+        localKatipNamespace (const $ ptrsKatipNamespace ptrs) $
+          runReaderT (rdrAct input) $ ptrsRunTree ptrs
+  putInStore f = flowToPTask $ putInStore f
+  getFromStore f = flowToPTask $ getFromStore f
+  internalManipulateStore f = flowToPTask $ internalManipulateStore f
