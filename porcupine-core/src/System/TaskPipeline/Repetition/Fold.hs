50d8b1c8baabdece95d8418ae4d37f74d75f20f7
Yves Parès (Ywen) <yves.pares@gmail.com>
2018-11-29 14:33:45 +0100

    FoldA exposes a type param to initialize the accumulator

--- porcupine-core/src/System/TaskPipeline/Repetition/Fold.hs
+++ porcupine-core/src/System/TaskPipeline/Repetition/Fold.hs
@@ -4,7 +4,9 @@
 {-# LANGUAGE TupleSections     #-}
 
 module System.TaskPipeline.Repetition.Fold
-  ( ptaskFold
+  ( module Control.Arrow.FoldA
+  , RepInfo(..)
+  , ptaskFold
   , unsafeGeneralizeM
   , foldlTask
   , foldStreamTask
@@ -25,7 +27,7 @@ import           System.TaskPipeline.Repetition.Internal
 
 -- | Turns a fold in some monad to a fold compatible with 'foldTask'
 unsafeGeneralizeM :: (KatipContext m)
-                  => FoldM m a b -> FoldA (PTask m) a b
+                  => FoldM m a b -> FoldA (PTask m) i a b
 unsafeGeneralizeM (FoldM step start done) =
   FoldA (unsafeLiftToPTask $ \(Pair a x) -> step a x)
         (unsafeLiftToPTask $ const start)
@@ -33,15 +35,13 @@ unsafeGeneralizeM (FoldM step start done) =
 
 -- | Creates a 'FoldA' from a 'PTask'.
 ptaskFold :: (Show idx, Monad m)
-          => RepInfo
-          -> PTask m (x,acc) acc
-          -> acc
-          -> FoldA (PTask m) (idx,x) acc
-ptaskFold ri step initAcc =
-  FoldA (arr onInput >>> makeRepeatable ri step >>> arr snd)
-        (pure initAcc) id
+          => RepInfo  -- ^ How to log the repeated task
+          -> PTask m (acc,input) acc -- ^ The folding task
+          -> FoldA' (PTask m) (idx,input) acc
+ptaskFold ri step =
+  FoldA (arr onInput >>> makeRepeatable ri step >>> arr snd) id id
   where
-    onInput (Pair acc (idx,x)) = (idx,(x,acc))
+    onInput (Pair acc (idx,x)) = (idx,(acc,x))
 
 -- | Consumes a Stream with a 'FoldA' created with 'ptaskFold', 'generalizeA',
 -- 'unsafeGeneralizeM', or a composition of such folds.
@@ -51,8 +51,8 @@ ptaskFold ri step initAcc =
 -- overwritten. See 'makeRepeatable'.
 foldStreamTask
   :: (KatipContext m)
-  => FoldA (PTask m) a b
-  -> PTask m (Stream (Of a) m r) (b, r)
+  => FoldA (PTask m) i a b
+  -> PTask m (i, Stream (Of a) m r) (b, r)
 foldStreamTask (FoldA step start done) =
   (reqs, runnable) ^. from splittedPTask
   where
@@ -61,18 +61,19 @@ foldStreamTask (FoldA step start done) =
     (reqsDone, runnableDone)   = done ^. splittedPTask
     reqs = reqsStart <> reqsStep <> reqsDone
     runnable =
-      id &&&& (pure () >>> runnableStart) >>> loopStep >>> first runnableDone
-    loopStep = proc (Pair stream acc) -> do
+      first runnableStart >>> loopStep >>> first runnableDone
+    loopStep = proc (acc, stream) -> do
       firstElem <- withRunnableState (const S.next) -< stream
       case firstElem of
         Left r -> returnA -< (acc, r)
         Right (a, stream') -> do
           acc' <- runnableStep -< Pair acc a
-          loopStep -< Pair stream' acc'
+          loopStep -< (acc', stream')
 
 -- | Consumes a list with a 'FoldA' over 'PTask'
 foldlTask
   :: (KatipContext m)
-  => FoldA (PTask m) a b
-  -> PTask m [a] b
-foldlTask fld = arr S.each >>> foldStreamTask fld >>> arr fst
+  => FoldA (PTask m) i a b
+  -> PTask m (i,[a]) b
+foldlTask fld = arr (\(i,l) -> (i,S.each l))
+            >>> foldStreamTask fld >>> arr fst
