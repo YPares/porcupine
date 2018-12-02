c436aa55ec843c2a57d51d747a5043d220b8cb9a
Yves Parès (Ywen) <yves.pares@gmail.com>
2018-12-02 20:03:03 +0100

    Added cookReaderSoup

--- porcupine-core/src/Control/Monad/ReaderSoup.hs
+++ porcupine-core/src/Control/Monad/ReaderSoup.hs
@@ -14,35 +14,53 @@ import Control.Monad.Trans.Reader
 import Data.Vinyl
 import Data.Vinyl.Derived
 import Data.Vinyl.Functor
+import Data.Vinyl.TypeLevel
 import GHC.TypeLits
 
 
+-- | Represents a set of Reader-like monads as a one-layer Reader that can grow
+-- and host more Readers, in a way that's more generic than creating you own
+-- application stack of Reader and implementing a host of MonadXXX classes,
+-- because each of these MonadXXX classes can be implemented once and for all
+-- for the ReaderSoup type.
 newtype ReaderSoup_ (record::((Symbol, *) -> *) -> [(Symbol, *)] -> *) ctxs m a = ReaderSoup
   { unReaderSoup ::
       ReaderT (record ElField ctxs) m a }
   deriving (Functor, Applicative, Monad, MonadTrans)
 
+-- | The type of 'ReaderSoup_' your application will eat
 type ReaderSoup = ReaderSoup_ ARec
-type CookingReaderSoup = ReaderSoup_ Rec
 
+-- | A 'ReaderSoup' ready to be eaten
+type CookedReaderSoup = ReaderSoup_ Rec
+
+-- | Turns a 'ReaderSoup' into something than is ready to be eaten (ran)
+cookReaderSoup :: (NatToInt (RLength ctxs))
+               => ReaderSoup ctxs m a
+               -> CookedReaderSoup ctxs m a
+cookReaderSoup (ReaderSoup (ReaderT act)) =
+  ReaderSoup $ ReaderT $ act . toARec
+
+-- | Extracts the first context so it can be eaten (ran)
 pickTopping :: (KnownSymbol l)
-            => CookingReaderSoup ( (l:::ctx) : ctxs ) m a
-            -> ReaderT ctx (CookingReaderSoup ctxs m) a
+            => CookedReaderSoup ( (l:::ctx) : ctxs ) m a
+            -> ReaderT ctx (CookedReaderSoup ctxs m) a
 pickTopping (ReaderSoup (ReaderT actInSoup)) =
   ReaderT $ \ctx1 -> ReaderSoup $
     ReaderT $ \ctxs -> actInSoup $ Field ctx1 :& ctxs
 
+-- | Once all ingredients have been eaten, leaves only the bowl (the base monad)
+finishBroth :: CookedReaderSoup '[] m a -> m a
+finishBroth (ReaderSoup (ReaderT act)) = act RNil
 
 
-type IsInSoup record ctxs m =
-  ( HasField record (NameInSoup m) ctxs ctxs (CtxInSoup m) (CtxInSoup m)
-  , RecElemFCtx record ElField )
+-- type IsInSoup record ctxs m =
+--   ( HasField record (NameInSoup m) ctxs ctxs (CtxInSoup m) (CtxInSoup m)
+--   , RecElemFCtx record ElField )
 
-class (KnownSymbol (NameInSoup m)) => MonadInSoup m where
-  type NameInSoup m :: Symbol
-  type CtxInSoup m :: *
-
-  -- createDefaultContext :: IO (CtxInSoup m)
+-- class (KnownSymbol (NameInSoup m)) => MonadInSoup m where
+--   type NameInSoup m :: Symbol
+--   type CtxInSoup m :: *
     
   -- liftInSoup :: () => m a -> ReaderSoup cts a
   -- contextField :: ElField (  )
