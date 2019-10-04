4ddf05c05ab8f81e388d1f5ff0e6cec6868f36e1
Yves Parès <yves.pares@gmail.com>
2019-10-04 10:48:46 +0200

    Added two small examples

---
+++ porcupine-core/examples/example0/Example0.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+
+import           Data.DocRecord
+import qualified Data.Text as T
+import           Porcupine.Run
+import           Porcupine.Serials
+import           Porcupine.Tasks
+
+
+generateTxt :: Int -> T.Text
+generateTxt n = T.replicate n "a"
+
+
+resultFile :: DataSink T.Text
+resultFile = dataSink ["result"] $
+  somePureSerial (PlainTextSerial (Just "txt"))
+
+myTask :: (LogThrow m) => PTask m () ()
+myTask =
+      getOption ["options"]
+                (docField @"text-length" (10::Int)
+                          "The length of the text to output")
+  >>> arr generateTxt
+  >>> writeData resultFile
+
+main :: IO ()
+-- main = simpleRunPTask myTask ()
+main = runLocalPipelineTask (FullConfig "example0" "config.yaml" "." ()) myTask ()
